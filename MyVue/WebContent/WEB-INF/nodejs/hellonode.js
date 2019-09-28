2f787c9901b4b574e0030e718b611e9c33e22139
hehf <hehf@yimidida.com>
2019-09-28 16:06:48 +0800

    vue常用

---
+++ MyVue/WebContent/WEB-INF/nodejs/hellonode.js
@@ -0,0 +1,11 @@
+for (var i = 0; i < 10; i++) {
+  setTimeout(function(){
+    console.log(i);
+  })
+}
+// 输出十个 10
+for (let j = 0; j < 10; j++) {
+  setTimeout(function(){
+    console.log(j);
+  })
+}
