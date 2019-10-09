efbb8285488ffd5893f9a5ba115153b4ed023a79
hehf <hehf@yimidida.com>
2019-10-09 16:58:08 +0800

    vue格式

--- MyVue/WebContent/WEB-INF/vueclient/vue-cli-demo/src/view/TableData.vue
+++ MyVue/WebContent/WEB-INF/vueclient/vue-cli-demo/src/view/TableData.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="mystyle">
     {{ defaultDate|formatDate}}
-      <el-date-picker  style="float:left;width: 200px;"
-        v-model="params.data"
-        type="date"
-        placeholder="选择日期">
-      </el-date-picker>
-    <el-select v-model="params.yes" placeholder="请选择类型"  style="float:left;width: 200px;">
+    <el-date-picker style="float:left;width: 200px;"
+                    v-model="params.data"
+                    type="date"
+                    placeholder="选择日期">
+    </el-date-picker>
+    <el-select v-model="params.yes" placeholder="请选择类型" style="float:left;width: 200px;">
       <el-option
         v-for="item in options"
         :key="item.value"
@@ -14,7 +14,7 @@
         :value="item.key">
       </el-option>
     </el-select>
-    <el-button type="success" @click="getData"  style="float:left;">查询</el-button>
+    <el-button type="success" @click="getData" style="float:left;">查询</el-button>
     <el-table
       :data="tableData"
       style="width: 100%">
@@ -39,15 +39,18 @@
 </template>
 
 <style scoped>
-  .mystyle{
+  .mystyle {
     border: blueviolet 1px solid;
   }
+
   .el-dropdown {
     vertical-align: top;
   }
+
   .el-dropdown + .el-dropdown {
     margin-left: 15px;
   }
+
   .el-icon-arrow-down {
     font-size: 12px;
   }
@@ -55,46 +58,49 @@
 </style>
 
 <script>
-  import axios from 'axios'
-  import dayjs from 'dayjs'
-  export default {
-    data() {
-      return {
-        tableData: [],
-        params:{},
-        dropdown:[],
-         options:[{key:1,value:'普通用户'},{key:2,value:"VIP"},{key:3,value:"管理员"}],
-        defaultDate:new Date()
+import axios from 'axios'
+import dayjs from 'dayjs'
+
+export default {
+  data() {
+    return {
+      tableData: [],
+      params: {},
+      dropdown: [],
+      options: [{key: 1, value: '普通用户'}, {key: 2, value: 'VIP'}, {key: 3, value: '管理员'}],
+      defaultDate: new Date()
+    }
+  },
+  methods: {
+    getData: function () {
+      // 校验
+      if (this.params.yes == null || this.params.yes === '') {
+        this.$message.error({message: '请选择查询条件', center: true})
+      } else {
+        this.$message.success({message: '正在查询,请稍后...', center: true})
       }
-    },
-    methods:{
-      getData:function(){
-        //校验
-        if(null==this.params.yes||this.params.yes==''){
-          this.$message.error({message: '请选择查询条件',center: true});
-        }else{
-          this.$message.success({message: '请求成功',center: true});
-        }
-        //后端请求数据
-        axios.get('/api/user/queryVIP?yes=' + this.params.yes)
-          .then(res => {
-            this.tableData = res.data.data;
-            // console.log(res)
-          }).catch(err => {
+      // 后端请求数据
+      axios.get('/api/user/queryVIP?yes=' + this.params.yes)
+        .then(res => {
+          this.tableData = res.data.data
+          this.$message.success({message: '请求成功', center: true})
+          // console.log(res)
+        }).catch(err => {
+          this.$message.error({message: '请求失败', center: true})
           console.log(err)
         })
-      },
-      formatDate:function(cellValue) {
-        var value=cellValue.createDate;
-        console.log("-------------------formatDate")
-        return null!=value?dayjs(value).format('YYYY-MM-DD HH:mm:ss'):null;
-      }
     },
-    filters: {//过滤器
-      formatDate:function(cellValue) {
-        console.log("-------------------filters")
-        return null!=cellValue?dayjs(cellValue).format('YYYY-MM-DD HH:mm:ss'):null;
-      }
-    },
+    formatDate: function (cellValue) {
+      var value = cellValue.createDate
+      console.log('-------------------formatDate')
+      return null != value ? dayjs(value).format('YYYY-MM-DD HH:mm:ss') : null
+    }
+  },
+  filters: {// 过滤器
+    formatDate: function (cellValue) {
+      console.log('-------------------filters')
+      return null != cellValue ? dayjs(cellValue).format('YYYY-MM-DD HH:mm:ss') : null
+    }
   }
+}
 </script>
