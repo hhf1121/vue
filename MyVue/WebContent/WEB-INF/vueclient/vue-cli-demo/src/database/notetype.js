b3dbdda844db63cbde28fddbcbb036cee0f19fe1
hehf <hehf@yimidida.com>
2020-07-10 17:07:14 +0800

    修改静态类型json

--- MyVue/WebContent/WEB-INF/vueclient/vue-cli-demo/src/database/notetype.js
+++ MyVue/WebContent/WEB-INF/vueclient/vue-cli-demo/src/database/notetype.js
@@ -1,1 +1,1 @@
-export const NoteType=[{"label":"衣服","value":1},{"label":"食物","value":2},{"label":"住宿","value":3},{"label":"旅行","value":4}];
+export const NoteType=[{"label":"衣服","value":1},{"label":"食物","value":2},{"label":"住宿","value":3},{"label":"旅行","value":4},{"label":"学习","value":5}];
