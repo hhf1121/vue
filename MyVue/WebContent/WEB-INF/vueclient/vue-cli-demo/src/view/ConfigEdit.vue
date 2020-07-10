fa4b9a31963614e4a85b1c0985f80051a0e5511c
hehf <hehf@yimidida.com>
2020-07-10 10:08:49 +0800

    花费类型配置-调整样式+颜色字段

--- MyVue/WebContent/WEB-INF/vueclient/vue-cli-demo/src/view/ConfigEdit.vue
+++ MyVue/WebContent/WEB-INF/vueclient/vue-cli-demo/src/view/ConfigEdit.vue
@@ -1,8 +1,9 @@
 <template>
   <div style="width: 800px;display: inline-block;align:center">
     <div style="float: left;position: relative;align:center">
-      <el-input v-model="input" placeholder="请输入新增项" style="width: 180px"></el-input>
-      <el-button type="success"  circle style="position: absolute;top: 0px;margin-left: 5px"  @click="addList(input)">＋</el-button>
+      <el-input v-model="input" placeholder="请输入新增项" style="width: 130px"></el-input>
+      <el-color-picker v-model="showColor" style="float: right"></el-color-picker>
+      <el-button type="success"  circle style="position: absolute;top: 0px;margin-left: 45px"  @click="addList(input)">＋</el-button>
     </div>
     <br>
     <br>
@@ -23,12 +24,21 @@
       return {
         data: [],//全部的数据
         value: [],//已选的数据的key
-        input:''
+        input:'',
+        showColor:''//颜色
       };
     },
     methods:{
       addList(input){
         if(input){
+          if(!this.showColor){
+            this.$alert('请为此配置添加一种统计图的展示颜色', '提示', {
+              confirmButtonText: '确定',
+              callback: action => {
+              }
+            });
+            return;
+          }
           //遍历，保存所有的key
           var max=this.data.map(o=>{
             return o.key;
@@ -43,8 +53,10 @@
           var obj={};
           obj.key=Math.max(...max)+1;//取最大的key+1
           obj.label=input;//名字是输入的
+          obj.color=this.showColor;
           this.data.push(obj);
           this.input='';
+          this.showColor='';
           //删除新增元素的选中状态
           // let index=this.value.indexOf(obj.input);
           // this.value.splice(index, 1);
@@ -56,7 +68,11 @@
         //   console.info(o);
         // });
         if(initKey.toString()==this.value.toString()){
-          this.$message.error({message:"请修改数据之后，再点保存",center:true});
+          this.$alert('数据未作修改,请先修改再保存', '提示', {
+            confirmButtonText: '我知道了',
+            callback: action => {
+            }
+          });
           return;
         }
         //根据已选key，保存相应的数据。
@@ -70,35 +86,39 @@
           if(res.success){
             this.$message.success({message:res.data,center:true});
             //重置初始选择
-            initKey=this.value;
+            // initKey=this.value;
+            this.init();
           }else {
             this.$message.error({message: res.error, center: true})
           }
         }).catch(err=>{
           this.$message.error({message: '请求错误', center: true})
         })
+      },
+      init(){
+        //数据字典
+        this.$api.getDataByConfigCode({"configCode":'cost_type'}).then(er=>{
+          if(er.success){
+            if(er.data.length>0){
+              this.data=er.data.map(o=>{
+                var object={};
+                object.label=o.typeLabel;
+                object.key=o.typeValue;
+                return object;
+              });
+              this.value=this.data.map(o=>{
+                return o.key;
+              })
+            }
+            initKey=this.value;
+          }
+        }).catch(err=>{
+          this.$message.error({message: '字典：[cost_type]请求错误', center: true})
+        })
       }
     },
     mounted(){
-      //数据字典
-      this.$api.getDataByConfigCode({"configCode":'cost_type'}).then(er=>{
-        if(er.success){
-          if(er.data.length>0){
-            this.data=er.data.map(o=>{
-              var object={};
-              object.label=o.typeLabel;
-              object.key=o.typeValue;
-              return object;
-            });
-            this.value=this.data.map(o=>{
-              return o.key;
-            })
-          }
-          initKey=this.value;
-        }
-      }).catch(err=>{
-        this.$message.error({message: '字典：[cost_type]请求错误', center: true})
-      })
+     this.init();
     }
   }
 </script>
--- MyVue/WebContent/WEB-INF/vueclient/vue-cli-demo/src/view/note/tendencyMap.vue
+++ MyVue/WebContent/WEB-INF/vueclient/vue-cli-demo/src/view/note/tendencyMap.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-      <el-form style="float: right"  :rules="rules" :model="ruleForm" ref="ruleForm">
-          <el-form-item  prop="queryData" label-width="80px" label="自">
-            <el-date-picker  type="date" v-model="ruleForm.queryData" :picker-options="pickerOptions" @blur="getInit('ruleForm')"></el-date-picker>
-            至<el-date-picker  type="date" v-model="ruleForm.nowData" disabled></el-date-picker>
-          </el-form-item>
-      </el-form>
-      <div id="chart2" style="width: 100%;height: 500px;padding-top: 50px;float: right"></div>
-    </div>
+  <div>
+    <el-form style="float: right"  :rules="rules" :model="ruleForm" ref="ruleForm">
+      <el-form-item  prop="queryData" label-width="80px" label="自">
+        <el-date-picker  type="date" v-model="ruleForm.queryData" :picker-options="pickerOptions" @blur="getInit('ruleForm')"></el-date-picker>
+        至<el-date-picker  type="date" v-model="ruleForm.nowData" disabled></el-date-picker>
+      </el-form-item>
+    </el-form>
+    <div id="chart2" style="width: 100%;height: 500px;padding-top: 50px;float: right"></div>
+  </div>
 </template>
 
 <script>
@@ -35,216 +35,164 @@
     yAxis: {
       type: 'value',//金额
     },
-    series: [
-      {
-        name:'',
-        data: [],//金额
-        type: 'line',
-        smooth: true,
-        color:'#b2149f'
-      },
-      {
-        name:'',
-        data: [],
-        type: 'line',
-        smooth: true,
-        color:'green'
-      },
-      {
-        name:'',
-        data: [],
-        type: 'line',
-        smooth: true,
-        color:'#b2b2b2'
-      },
-      {
-        name:'',
-        data: [],
-        type: 'line',
-        smooth: true,
-        color: 'red'
-      }/*,
-      {
-        name:'当日总额',
-        data: [1,2,3,4,5],
-        type: 'line',
-        smooth: true,
-        color: '#b2074c'
-      }*/
-    ]
+    series: []
   };
-    export default {
-        name: "tendencyMap",
-      data(){
-          //回调函数validateTime，中的this指的是它本身，并非vue对象。此处用_that替代
-          let _that=this;
-        let validateTime = (rule, value, callback) => {
-          let now = _that.ruleForm.nowData;
-          if (value === undefined || value === '' || value ===null) {
-            callback(new Error('请选择起始时间'));
-          }else if(value>=now ){
-            callback(new Error('起始日期必须在当前日期之前'));
-          }else{
-            //去后端校验
-            _that.$api.getAllNote({"from":_that.ruleForm.queryData,"now":_that.ruleForm.nowData}).then(re=>{
-              if(!re.success){
-                callback(new Error(re.error+""));
-              }else{
-                callback();
-              }
-            }).catch(err=>{
-              callback(new Error('请求后端异常'));
-            })
-          }
-        };
-          return{
-            rules: {//提交校验
-              queryData: [
-                {required: true, message: '请输入起始时间', trigger: 'blur'},
-                {validator : validateTime, type:'date',  trigger: 'blur' }
-              ]
-            },
-            pickerOptions: {
-              disabledDate(time) {
-                // return time.getTime() > Date.now();
-                return false;
-              },
-              shortcuts: [{
-                text: '昨天',
-                onClick(picker) {
-                  const date = new Date();
-                  date.setTime(date.getTime() - 3600 * 1000 * 24);
-                  picker.$emit('pick', date);
-                }
-              }, {
-                text: '一周前',
-                onClick(picker) {
-                  const date = new Date();
-                  date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
-                  picker.$emit('pick', date);
-                }
-              }, {
-                text: '一月前',
-                onClick(picker) {
-                  const date = new Date();
-                  date.setTime(date.getTime() - 3600 * 1000 * 24 * 30);
-                  picker.$emit('pick', date);
-                }
-              }, {
-                text: '一年前',
-                onClick(picker) {
-                  const date = new Date();
-                  date.setTime(date.getTime() - 3600 * 1000 * 24 * 365);
-                  picker.$emit('pick', date);
-                }
-              }]
-            },
-            ruleForm:{
-              queryData:new Date(new Date().setTime(new Date().getTime()-3600 * 1000 * 24 * 7)),
-              nowData:new Date()
-            }
-          }
-      },
-      methods:{
-        getInit(formName){//失焦请求
-          this.$refs[formName].validate((valid) => {
-            if (valid) {
-              this.init();
-            } else {
-              console.log('error submit!!')
-              return false
-            }
-          })
-        },
-        init(){
-          debugger
-          var init = echarts.init(document.getElementById("chart2"));
-          //路由赋值
-          let noteStr=this.NoteType;//label、value：名称、值
-          //数据字典
-          this.$api.getDataByConfigCode({"configCode":'cost_type'}).then(er=>{
-            if(er.success){
-              if(er.data.length>0){
-                  noteStr=er.data.map(o=>{
-                  var object={};
-                  object.label=o.typeLabel;
-                  object.value=o.typeValue;
-                  return object;
-                });
-              }
-            }
-          }).catch(err=>{
-            this.$message.error({message: '字典：[cost_type]请求错误', center: true})
-          })
-          let data=[];
-          this.$api.getAllNote({"from":this.ruleForm.queryData,"now":this.ruleForm.nowData}).then(re=>{
+  export default {
+    name: "tendencyMap",
+    data(){
+      //回调函数validateTime，中的this指的是它本身，并非vue对象。此处用_that替代
+      let _that=this;
+      let validateTime = (rule, value, callback) => {
+        let now = _that.ruleForm.nowData;
+        if (value === undefined || value === '' || value ===null) {
+          callback(new Error('请选择起始时间'));
+        }else if(value>=now ){
+          callback(new Error('起始日期必须在当前日期之前'));
+        }else{
+          //去后端校验
+          _that.$api.getAllNote({"from":_that.ruleForm.queryData,"now":_that.ruleForm.nowData}).then(re=>{
             if(!re.success){
-              // this.$message.error({message: re.error+"", center: true});
-              return;
+              callback(new Error(re.error+""));
+            }else{
+              callback();
             }
-            data=re.data;
-            //noteType 、noteMoney、timeStr
-            //拼装数据：每个类型一组。（展现：某项类型，花费的全部金额、都一个日期list）
-            //1.赋值x：日期
-            option.xAxis.data=data.map(e=>{
-              return e.timeStr;
-            });
-            //2.赋值类型
-            option.legend.data=noteStr.map(e=>{
-              return e.label;
-            });
-            for (var i=0;i<option.series.length;i++){
-              for (let j = 0; j < noteStr.length; j++) {
-                if(i==j){
-                  option.series[i].name=noteStr[j].label;
-                  break;
-                }
-              }
-            }
-            //4.根据类型、分类
-            var seriesData=option.series;
-            for (let i = 0; i < seriesData.length; i++) {
-              if(option.series[i].name=='衣服'){
-                option.series[i].data=data.map(e=>{
-                  return e.yfcount;
-                })
-              }
-              if(option.series[i].name=='食物'){
-                option.series[i].data=data.map(e=>{
-                  return e.swcount;
-                })
-              }
-              if(option.series[i].name=='住宿'){
-                option.series[i].data=data.map(e=>{
-                  return e.zscount;
-                })
-              } if(option.series[i].name=='旅行'){
-                option.series[i].data=data.map(e=>{
-                  return e.lxcount;
-                })
-              }
-              //总金额
-              // {name:'', data: [],type: 'line', smooth: true, color:'#b2149f'}
-              // option.series.push({
-              //   name:'当日总额', data: [],type: 'line', smooth: true, color:'#b2149f'
-              // });
-            }
-            // 使用刚指定的配置项和数据显示图表。
-            init.setOption(option);
           }).catch(err=>{
-            this.$message.error({message: '请求失败', center: true})
-            return;
+            callback(new Error('请求后端异常'));
           })
         }
-      },
-      mounted(){
-          this.init();
-      },
-      destroyed(){
-
+      };
+      return{
+        rules: {//提交校验
+          queryData: [
+            {required: true, message: '请输入起始时间', trigger: 'blur'},
+            {validator : validateTime, type:'date',  trigger: 'blur' }
+          ]
+        },
+        pickerOptions: {
+          disabledDate(time) {
+            // return time.getTime() > Date.now();
+            return false;
+          },
+          shortcuts: [{
+            text: '昨天',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24);
+              picker.$emit('pick', date);
+            }
+          }, {
+            text: '一周前',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', date);
+            }
+          }, {
+            text: '一月前',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit('pick', date);
+            }
+          }, {
+            text: '一年前',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 365);
+              picker.$emit('pick', date);
+            }
+          }]
+        },
+        ruleForm:{
+          queryData:new Date(new Date().setTime(new Date().getTime()-3600 * 1000 * 24 * 7)),
+          nowData:new Date()
+        }
       }
+    },
+    methods:{
+      getInit(formName){//失焦请求
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            this.init();
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
+      },
+      init(){
+        var init = echarts.init(document.getElementById("chart2"));
+        option.legend.data=[];
+        option.series=[];
+        option.xAxis.data=[];
+        //数据字典
+        this.$api.getDataByConfigCode({"configCode":'cost_type'}).then(er=>{
+          if(er.success){
+            if(er.data.length>0){
+              option.legend.data=er.data.map(o=>{
+                return o.typeLabel;
+              });
+              //路由赋值
+              let data=[];
+              this.$api.getAllNote({"from":this.ruleForm.queryData,"now":this.ruleForm.nowData}).then(re=>{
+                if(!re.success||re.data.length==0){
+                  init.setOption(option);
+                  this.$message.error({message: '此时间段，没有记录信息', center: true})
+                  return;
+                }
+                data=re.data;
+                //noteType 、noteMoney、timeStr
+                //拼装数据：每个类型一组。（展现：某项类型，花费的全部金额、都一个日期list）
+                //1.赋值x：日期
+                option.xAxis.data=data.map(e=>{
+                  return e.timeStr;
+                });
+                //2.赋值类型
+                option.legend.data=data[0].lists.map(o=>{
+                  return o.name;
+                });
+                //4.根据类型、分类
+                option.series=[];
+                for (let i = 0; i < data[0].lists.length; i++ ) {
+                  var obj={};
+                  obj.name=data[0].lists[i].name;
+                  obj.type='line';
+                  obj.smooth=true;
+                  obj.color=data[0].lists[i].color;
+                  obj.data=[];
+                  option.series.push(obj);
+                }
+                //处理一组数据(类型)
+                for (let xx = 0; xx < data.length; xx++) {
+                  for(let yy=0;yy<data[xx].lists.length;yy++){
+                    for (let zz=0;zz<option.series.length;zz++){
+                      if(option.series[zz].name==data[xx].lists[yy].name){
+                        option.series[zz].data.push(data[xx].lists[yy].count);
+                      }
+                    }
+                  }
+                }
+                // 使用刚指定的配置项和数据显示图表。
+                init.setOption(option);
+              }).catch(err=>{
+                this.$message.error({message: '请求失败', center: true})
+                return;
+              })
+            }
+          }
+        }).catch(err=>{
+          this.$message.error({message: '字典：[cost_type]请求错误', center: true})
+        });
+      }
+    },
+    mounted(){
+      this.init();
+    },
+    destroyed(){
 
     }
+
+  }
 </script>
 
 <style scoped>
